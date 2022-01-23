05809536147fc92be3c1516fcb087a1e09f8b47b
Priyankanath176 <74127901+Priyankanath176@users.noreply.github.com>
2022-01-23 15:55:09 +0530

    Create app.js

---
+++ client/src/app.js
@@ -0,0 +1,13 @@
+import react from "react";
+import HomeLayoutHOC from "./HOC/Home.HOC";
+import Temp from "./Components/temp"
+
+function App() {
+  return (
+  <>
+  <HomeLayoutHOC path = "/" exact component={Temp}/>
+  </>
+  );
+}
+
+export default App;
